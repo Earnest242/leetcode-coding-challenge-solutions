a9631c011124bda8b933bdc99199caf0bdca1f18
Earnest <earnestmuch@gmail.com>
2022-10-28 16:00:54 +0300

    coding challenge

---
+++ baseball_game/baseball_game.js
@@ -0,0 +1,33 @@
+//function to perform the operation
+var calPoints = function(ops){
+    var result =null;
+    let stack1 = []
+    for (let i in ops) {
+        if (ops[i] == "+"){
+            let p = stack1[stack1.length-1] + stack1[stack1.length-2]
+            stack1.push(p)
+        }
+        else if (ops[i] == "C"){
+            stack1.pop()
+        }
+        else if (ops[i] == "D"){
+            let n = stack1[stack1.length-1]*2
+            stack1.push(n)
+        }
+        else {
+            stack1.push(Number(ops[i]))
+        }
+
+    }
+    for (let f in stack1){
+        result =result + stack1[f]
+    }
+    
+    
+    return result;
+}
+//insert different test cases into this array
+var ops3 =["5","2","C","D","+"]
+
+//calling the calPoints function and outputting the result
+console.log(calPoints(ops3))
